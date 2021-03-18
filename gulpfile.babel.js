bed0f0cc30d4a54afd0e8436f506e07b9ff21693
PavelKaraev <kraev.pashka@gmail.com>
2021-03-18 14:05:10 +0200

    First Commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,81 @@
+"use strict";
+
+import gulp from "gulp";
+
+const requireDir = require("require-dir"),
+    paths = {
+        html: {
+            src: [
+                "./src/**/*.html",
+                "./src/*.html"
+            ],
+            dist: "./dist/",
+            watch: [
+                "./src/**/*.html"
+            ]
+        },
+        styles: {
+            src: "./src/styles/main.{scss,sass}",
+            dist: "./dist/styles/",
+            watch: [
+                "./src/styles/**/*.{scss,sass}"
+            ]
+        },
+        scripts: {
+            src: "./src/js/index.js",
+            dist: "./dist/js/",
+            watch: [
+                "./src/js/**/*.js"
+            ]
+        },
+        images: {
+            src: [
+                "./src/img/**/*.{jpg,jpeg,png,gif,tiff,svg}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ],
+            dist: "./dist/img/",
+            watch: "./src/img/**/*.{jpg,jpeg,png,gif,svg,tiff}"
+        },
+        webp: {
+            src: [
+                "./src/img/**/*.{jpg,jpeg,png,tiff}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ],
+            dist: "./dist/img/",
+            watch: [
+                "./src/img/**/*.{jpg,jpeg,png,tiff}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ]
+        },
+        sprites: {
+            src: "./src/img/svg/*.svg",
+            dist: "./dist/img/sprites/",
+            watch: "./src/img/svg/*.svg"
+        },
+        fonts: {
+            src: "./src/fonts/**/*.{woff,woff2}",
+            dist: "./dist/fonts/",
+            watch: "./src/fonts/**/*.{woff,woff2}"
+        },
+        favicons: {
+            src: "./src/img/favicon/*.{jpg,jpeg,png,gif}",
+            dist: "./dist/img/favicons/",
+        },
+        gzip: {
+            src: "./src/.htaccess",
+            dist: "./dist/"
+        }
+    };
+
+requireDir("./gulp-tasks/");
+
+export { paths };
+
+export const development = gulp.series("clean",
+    gulp.parallel(["html", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons"]),
+    gulp.parallel("serve"));
+
+export const prod = gulp.series("clean",
+    gulp.series(["html", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons", "gzip"]));
+
+export default development;
